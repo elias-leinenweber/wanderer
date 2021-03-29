e59bfce1bed8d566175fa6ab3d10be4959cef62e
Elias Leinenweber <elias.leinenweber@etu.unistra.fr>
2021-03-29 16:12:22 +0200

    Passé la map en propriété du contrôle

--- view/Controls/MapView.cs
+++ view/Controls/MapView.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows.Forms;
+﻿using System.ComponentModel;
+using System.Drawing.Design;
+using System.Windows.Forms;
+using System.Windows.Forms.Design;
 
 namespace Wanderer.view.Controls
 {
@@ -6,19 +9,27 @@ namespace Wanderer.view.Controls
     {
         private TileView[,] tileViews;
 
-        public MapView(Map map)
+        [Category("Model")]
+        [Browsable(true)]
+        [Description("La map correspondante.")]
+        [Editor(typeof(WindowsFormsComponentEditor), typeof(UITypeEditor))]
+        public Map Model {
+            set {
+                ColumnCount = value.Tiles.GetLength(0);
+                RowCount = value.Tiles.GetLength(1);
+
+                for (int i = 0; i < ColumnCount; ++i)
+                    for (int j = 0; j < RowCount; ++j)
+                    {
+                        tileViews[i, j] = new TileView(value.Tiles[i, j]);
+                        Controls.Add(tileViews[i, j], i, j);
+                    }
+            }
+        }
+
+        public MapView()
         {
             InitializeComponent();
-            
-            ColumnCount = map.Tiles.GetLength(0);
-            RowCount = map.Tiles.GetLength(1);
-
-            for (int i = 0; i < ColumnCount; ++i)
-                for (int j = 0; j < RowCount; ++j)
-                {
-                    tileViews[i, j] = new TileView(map.Tiles[i, j]);
-                    Controls.Add(tileViews[i, j], i, j);
-                }
         }
     }
 }
