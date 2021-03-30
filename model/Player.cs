018c1b371d98326b1f9aca4d2a082be98da8581d
Emma Sattler <e.sattler@etu.unistra.fr>
2021-03-31 00:02:45 +0200

    Fix des prix

--- model/Player.cs
+++ model/Player.cs
@@ -7,9 +7,10 @@ namespace Wanderer.model
         public Color Color { get; set; }
         public int bonusCh = 0;//bonus Chemin
         public int bonusCl = 0; //bonus Club
-        public int marks = 40;
+        public int marks = 50;
         public int randonneurs = 0;
         public int tailleTerritoire;
+        public int gain = 0;
 
         public Player(Color color)
         {
