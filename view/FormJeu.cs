c79f6df68164a1d77d8fd6c42cd076b970bfa48f
Emma Sattler <e.sattler@etu.unistra.fr>
2021-03-30 21:14:51 +0200

    Cleanup formFin

--- view/FormJeu.cs
+++ view/FormJeu.cs
@@ -14,27 +14,27 @@ namespace Wanderer.view
 {
     public partial class FormJeu : Form
     {
-        /*bool gareCreated = true;
-        int nombreMarks;
-        int nombreRandonneurs;*/
         int tailleTerritoire;
         int nombreTour;
         int bonusCh = 0;//bonus Chemin
         int bonusCl = 0; //bonus Club
-        /*int nombreUsine;
-        int nombreRefuge;
-        int nombreChemin;
-        int nombreClub;
-        int bonus = 0;*/
-
-
-        /*Ajout de Emma 
-         * */
         int marks = 40;
         int randonneurs = 0;
+
+        const int limiteRandonneur = 100;
+        const int limiteTerritoire = 200;
+        
         int[] tbCouts = new int[] { 30, 10, 15, 10, 60 }; //Refuge,Chemin,Club,Train,Usine
         int[] tbRandonneur = new[] { 3, 2, 6, 0, -10 };
         int[] tbConstruct = new int[]{0, 0, 0, 0, 0}; //Refuge,Chemin,Club,Train,Usine
+
+        /**
+         * Action updateButtons()
+         *   - vérifie si fond suffisant pour les éléments
+         *      - si oui, enable + fond transparent
+         *      - si non, disable + fond gris
+         *   - premier tour grise tout sauf btn Train
+         *  */
         public void updateButtons()
         {
             foreach (Object o in Controls)
@@ -69,6 +69,14 @@ namespace Wanderer.view
                 btnTrain.Enabled = false;
             }
         }
+
+        /**
+         * Action btnClick
+         *  - vérifie si fond suffisant
+         *  - mise en place de l'image
+         *  - calculs des bonus des chemins et club
+         *  - lance update()
+         **/
 
         private void btn_Click(object sender, EventArgs e)
         {
@@ -116,21 +124,8 @@ namespace Wanderer.view
             updateButtons();
         }
 
-        /*/Fin des ajouts*/
 
-
-        const int limiteRandonneur = 100;
-        const int limiteTerritoire = 200;
-
-        public void updateNombreMarks(int nombreUsine, int nombreRefuge)
-        {
-           // nombreMarks = nombreUsine * 5 + nombreRefuge * 5 + 10 + nombreMarks;
-        }
-
-        public void updateNombreRandonneurs(int nombreClub, int nombreChemins, int nombreRefuges)
-        {
-           // nombreRandonneurs = nombreClub * 6 + nombreChemins * 2 + nombreRefuges * 3 - 10 * nombreUsine + bonus;
-        }
+        
 
         public void updateTailleTerritoire(int nombreClub, int nombreChemins, int nombreRefuges)
         {
@@ -140,26 +135,6 @@ namespace Wanderer.view
         public void disable()
         {
             this.Enabled = false;
-        }
-
-
-        public void updateLabels()
-        {
-           /* if(nombreTour > 0)
-            {
-                updateNombreMarks(nombreUsine, nombreRefuge);
-            }
-            updateNombreRandonneurs(nombreClub, nombreChemin, nombreRefuge);
-            updateTailleTerritoire(nombreClub, nombreChemin, nombreRefuge);
-
-            lblArgent.Text = "Argent :" + nombreMarks;
-            lblMarcheur.Text = "Marcheurs : " + nombreRandonneurs;
-            lblTour.Text = "Nombre d'action : " + nombreTour++;
-            if (MapView.SelectedTile != null && MapView.SelectedTile.model.HasChanged)
-            {
-                MapView.SelectedTile.Image = image();
-                MapView.SelectedTile.model.HasChanged = false;
-            }*/
         }
 
         public FormJeu()
@@ -176,9 +151,7 @@ namespace Wanderer.view
         private void FormJeu_Load(object sender, EventArgs e)
         {
             mapView1.Model = new Map(20, 10);
-            //nombreMarks = 40;
             update();
-            //updateLabels();
         }
 
         private void FormJeu_FormClosed(object sender, FormClosedEventArgs e)
@@ -207,64 +180,23 @@ namespace Wanderer.view
 
         private void btnUsine_Click(object sender, EventArgs e)
         {
-            /*if (nombreMarks < 60) return;
-            MapView.SelectedTile.model.Improvement = Improvement.Factory;
-            nombreUsine++;
-            nombreMarks -= 40;
-            updateLabels();*/
         }
 
         private void btnTrain_Click(object sender, EventArgs e)
         {
-            /*if(nombreMarks >= 10 && gareCreated)
-            {
-                MapView.SelectedTile.model.Improvement = Improvement.TrainStation;
-                gareCreated = false;
-                nombreMarks -= 10;
-                updateLabels();
-            }*/
         }
 
         private void btnRefuge_Click(object sender, EventArgs e)
         {
-            /*if (nombreRefuge % 5 == 0 && nombreRefuge != 0)
-            {
-                bonus += 5;
-            }
-            if (nombreMarks >= 30)
-            {
-                MapView.SelectedTile.model.Improvement = Improvement.Refuge;
-                nombreRefuge++;
-                nombreMarks -= 30;
-                updateLabels();
-            }*/
         }
 
 
         private void btnChemin_Click(object sender, EventArgs e)
         {
-            /*if (nombreChemin % 5 == 0 && nombreRefuge != 0)
-            {
-                bonus += 2;
-            }
-            if (nombreMarks >= 10)
-            {
-                MapView.SelectedTile.model.Improvement = Improvement.Path;
-                nombreChemin++;
-                nombreMarks -= 10;
-                updateLabels();
-            }*/
         }
 
         private void btnClub_Click(object sender, EventArgs e)
         {
-            /*if(nombreMarks >= 15)
-            {
-                MapView.SelectedTile.model.Improvement = Improvement.Club;
-                nombreClub++;
-                nombreMarks -= 10;
-                updateLabels();
-            }*/
         }
 
         private void mapView1_Paint(object sender, PaintEventArgs e)
